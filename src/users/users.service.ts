69a939a5057a36105e376029fbc989c6e64f678d
Geziel Carvalho <geziel.natal@gmail.com>
2022-09-28 21:22:09 -0300

    Not found exception

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -45,7 +45,7 @@ export class UsersService {
     public async update(id: number, attrs: Partial<User>) {
         const user = await this.repo.findOneBy({id});
         if (!user) {
-            throw new Error('User not found');
+            throw new NotFoundException('User not found');
         }
         Object.assign(user, attrs);
         return this.repo.save(user);
@@ -57,7 +57,7 @@ export class UsersService {
     public async remove(id: number) {
         const user = await this.repo.findOneBy({id});
         if (!user) {
-            throw new Error('User not found');
+            throw new NotFoundException('User not found');
         }
         return this.repo.remove(user);
     }
