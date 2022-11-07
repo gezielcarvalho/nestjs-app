e9316a4fc55fbedc4753a870faf920f4e6f3b91f
Geziel Carvalho <geziel.natal@gmail.com>
2022-11-07 14:38:12 -0300

    Signing in a User.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -28,13 +28,17 @@ export class UsersController {
      * createUser
      */
     @Post('/signup')
-    public createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Post('/signin')
-    public signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     /**
